91a558d1f40a5b6ce424b4141398ff406d67894f
HashJungle <zhouxiangxin1998@qq.com>
2020-10-03 00:33:20 +0800

    add extract data

---
+++ myscripts/extract_generated.py
@@ -0,0 +1,34 @@
+import argparse
+from tqdm import tqdm
+import os
+
+parser = argparse.ArgumentParser(description=("extract"))
+parser.add_argument('--input_dir', type=str, help='/path/to/generate_{train/test}.txt')
+parser.add_argument('--output_dir', type=str, help='/path/to/raw/')
+args = parser.parse_args()
+setattr(args, "src", os.path.join(args.tgt_dir, "train.src-trg.src"))
+setattr(args, "trg", os.path.join(args.tgt_dir, "train.src-trg.trg"))
+
+with open(args.input_dir, 'r') as input_file:
+	lines = input_file.readlines()
+
+def safe_index(toks, index, default):
+	try:
+		return toks[index]
+	except IndexError:
+		return default
+
+with open(args.src, 'w') as src_file, open(args.trg, 'w') as trg_file:
+	for line in tqdm(lines):
+		line = line.strip()
+		if line.startswith('S-'):
+			src = safe_index(line.rstrip().split('\t'), 1, '')
+		elif line.startswith('H-'):
+			if src is not None:
+				trg = safe_index(line.rstrip().split('\t'), 2, '')
+				# if validate(src, tgt):
+				# 	print(src, file=src_h)
+				# 	print(tgt, file=tgt_h)
+				src_file.write(src + '\n')
+				trg_file.write(trg + '\n')
+				src = None
---
+++ myscripts/extract_generated.sh
@@ -0,0 +1,3 @@
+python extract_generated.py \
+  --input_dir /mnt/xiangxin2/data/generated/generate-train.txt \
+  --output_dir /mnt/xiangxin2/data/generated/preprocessed
