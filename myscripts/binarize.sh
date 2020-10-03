035c41301d67ff8b68248614a965cc2013e087e9
HashJungle <zhouxiangxin1998@qq.com>
2020-10-03 11:19:46 +0800

    add binarize.sh

---
+++ myscripts/binarize.sh
@@ -0,0 +1,8 @@
+INPUT_DIR=/mnt/xiangxin2/data/generated/extracted
+OUTPUT_DIR=/mnt/xiangxin2/data/generated/preprocessed
+fairseq-preprocess \
+    --source-lang src --target-lang trg \
+    --trainpref $TEXT/train.src-trg \
+    -tgtdict /mnt/xiangxin/seq2seq_char/preprocessed_data/dict.src.txt \
+    --destdir ${OUTPUT_DIR} --thresholdtgt 0 --thresholdsrc 0 \
+    --workers 20
