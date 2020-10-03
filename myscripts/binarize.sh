1b8e523fa12354b68c73426dc37344146a5d9886
HashJungle <zhouxiangxin1998@qq.com>
2020-10-03 11:25:42 +0800

    add joined-dictionary

--- myscripts/binarize.sh
+++ myscripts/binarize.sh
@@ -1,8 +1,9 @@
 INPUT_DIR=/mnt/xiangxin2/data/generated/extracted
-OUTPUT_DIR=/mnt/xiangxin2/data/generated/preprocessed
+OUTPUT_DIR=/mnt/xiangxin2/data/generated/preprocessed-joined
 fairseq-preprocess \
     --source-lang src --target-lang trg \
     --trainpref ${INPUT_DIR}/train.src-trg \
     --tgtdict /mnt/xiangxin/seq2seq_char/preprocessed_data/dict.src.txt \
+    --joined-dictionary \
     --destdir ${OUTPUT_DIR} --thresholdtgt 0 --thresholdsrc 0 \
     --workers 20
