eb9543a369ef86f7dd15f2c378c1f926c24501df
HashJungle <zhouxiangxin1998@qq.com>
2020-10-03 11:21:50 +0800

    fix bug in binarize.sh

--- myscripts/binarize.sh
+++ myscripts/binarize.sh
@@ -2,7 +2,7 @@ INPUT_DIR=/mnt/xiangxin2/data/generated/extracted
 OUTPUT_DIR=/mnt/xiangxin2/data/generated/preprocessed
 fairseq-preprocess \
     --source-lang src --target-lang trg \
-    --trainpref $TEXT/train.src-trg \
-    -tgtdict /mnt/xiangxin/seq2seq_char/preprocessed_data/dict.src.txt \
+    --trainpref ${INPUT_DIR}/train.src-trg \
+    --tgtdict /mnt/xiangxin/seq2seq_char/preprocessed_data/dict.src.txt \
     --destdir ${OUTPUT_DIR} --thresholdtgt 0 --thresholdsrc 0 \
     --workers 20
