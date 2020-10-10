34b99ed83b9fe2fc44e432e9e2869a64c2fde82b
HashJungle <zhouxiangxin1998@qq.com>
2020-10-10 08:16:38 +0800

    add generate_naive.sh

---
+++ myscripts/IWSLT14/generate_naive.sh
@@ -0,0 +1,9 @@
+DATA_DIR=/mnt/xiangxin2/data/iwslt14/preprocessed
+SAVE_DIR=/mnt/xiangxin2/data/iwslt14/checkpoints/naive
+RESULT_DIR=/mnt/xiangxin2/data/iwslt14/checkpoints/naive/generated
+
+fairseq-generate ${DATA_DIR} \
+    --path ${SAVE_DIR}/checkpoint_best.pt \
+    --batch-size 128 --beam 5 \
+    --gen-subset train \
+    --results-path ${RESULT_DIR}
