79463fe4fb6169221ec1a05fdf6f2a81e67af4f8
xx-zhou16 <zhouxiangxin1998@qq.com>
2020-10-10 04:14:21 +0000

    add disitll from 4L

--- myscripts/IWSLT14/distill_from_4L.sh
+++ myscripts/IWSLT14/distill_from_4L.sh
@@ -1,5 +1,5 @@
 exp_name=disitll_2L_from_4L
-DATA_DIR=/mnt/xiangxin2/data/iwslt14/checkpoints/naive/generated/preprocessed
+DATA_DIR=/mnt/xiangxin2/data/iwslt14/checkpoints/naive_4L/generated/preprocessed
 SAVE_DIR=/mnt/xiangxin2/data/iwslt14/checkpoints/${exp_name}
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train \
