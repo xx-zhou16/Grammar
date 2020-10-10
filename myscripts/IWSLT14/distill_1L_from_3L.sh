9115ea64f6e5a648a2a536ae38b59867de601891
HashJungle <zhouxiangxin1998@qq.com>
2020-10-10 23:36:46 +0800

    add distill_1L_from_3L.sh

---
+++ myscripts/IWSLT14/distill_1L_from_3L.sh
@@ -0,0 +1,19 @@
+exp_name=disitll_1L_from_3L
+DATA_DIR=/mnt/xiangxin2/data/iwslt14/checkpoints/naive_3L/generated/preprocessed
+SAVE_DIR=/mnt/xiangxin2/data/iwslt14/checkpoints/${exp_name}
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train \
+    ${DATA_DIR} \
+    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+    --encoder-layers 1 --decoder-layers 1 \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --dropout 0.3 --weight-decay 0.0001 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --max-tokens 4096 \
+    --eval-bleu \
+    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-detok moses \
+    --eval-bleu-remove-bpe \
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
+    --save-dir ${SAVE_DIR}
